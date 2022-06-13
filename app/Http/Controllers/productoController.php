eddcca6bd76d0396ea6feb02530c7ffb60054bc9
Your Name <you@example.com>
2022-06-13 17:35:56 -0500

    Mensaje

--- app/Http/Controllers/productoController.php
+++ app/Http/Controllers/productoController.php
@@ -6,6 +6,8 @@ use App\Models\Producto;
 use App\Models\Categoria;
 use App\models\Marca;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class productoController extends Controller
 {
@@ -45,17 +47,61 @@ class productoController extends Controller
      */
     public function store(Request $r)
     {
-     //crear nuevo producto 
-     $p = new Producto();
-     $p->Nombre = $r->Nombre;
-     $p->Desc = $r->Desc;
-     $p->Precio = $r->Precio;
-     $p->marca_id = $r->marca;
-     $p->categoria_id = $r->categoria;
-     //grabar producto
-     $p->save();
-     echo"producto gurdado";
 
+        //validaciones
+        //1.establecer reglas de validacion
+        $reglas=[
+            "nombre" => 'required|alpha|unique:productos,nombre',
+            "desc" => 'required|min:5|max:20',
+            "precio" => 'required|numeric', 
+            "imagen" => 'required|image',
+            "marca" => 'required|',
+            "categoria" => 'required|'
+        ];
+
+        //2. crear el objeto validador
+        $v =Validator::make($r->all() , $reglas );
+
+        //3. validar
+        if($v->fails()){
+            //si la validacion fallo
+            //redirigirme a la vista de create(ruta: productos/create)
+            return redirect('productos/create')
+            ->withErrors($v);
+        }else{
+
+            //validacion exitosa
+            $archivo=$r->imagen;
+
+            //obtener el nombre original del arcivo
+             $nombre_archivo = ($archivo->getClientOriginalName());
+             
+              //establecer la ubicacion de guardado del archivo
+         
+              $ruta = public_path()."/img";
+             
+             //mover el archivo de imagen a la ubicacion y nombre deseado
+          $archivo->move($ruta , $nombre_archivo);
+            
+             
+          //crear nuevo producto 
+          $p = new Producto();
+          $p->Nombre = $r->nombre;
+          $p->Desc = $r->desc;
+          $p->Precio = $r->precio;
+          $p->marca_id = $r->marca;
+          $p->categoria_id = $r->categoria;
+          $p->Imagen =$nombre_archivo;
+     
+          //grabar producto
+          $p->save();
+          //redirigir a productos/create
+          //con un mensajede exito
+          return redirect('productos/create')
+                 ->with('mensajito' , 'producto registro exitosamente');
+        }
+        
+         
     }
 
     /**
--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -3,8 +3,16 @@
 @section('contenido')
     <form class="col s12"
      method="POST"
-      action="{{route('productos.store')}}">
+      action="{{route('productos.store')}}"
+      enctype="multipart/form-data"
+      >
 @csrf
+
+    @if( session('mensajito') )
+      <div class="row">
+         <strong>{{ session('mensajito') }}</strong>
+         </div>
+    @endif
 
     <div class="row">
     <div class="col s8">
@@ -13,24 +21,37 @@
     </div>
     <div class="row">
         <div class="input-field col s6">
-          <input placeholder="Nombre Del Producto" id="Nombre" for="first_name" type="text" class="validate" name="Nombre">
+          <input placeholder="Nombre Del Producto" id="Nombre" for="first_name" type="text" class="validate" name="nombre">
           <label for="Nombre">Nombre del producto</label>
+          <strong>{{ $errors->first('nombre') }}</strong>
         </div>
         <div class="input-field col s6">
-          <input id="desc" type="text" class="validate" name="Desc">
+          <input id="desc" type="text" class="validate" name="desc">
           <label for="last_name">Descripcion</label>
+          <strong class="#d81b60 pink darken-1 ">{{ $errors->first('desc') }}</strong>
         </div>
       </div>
+
+      <div class="row">
+        <div class="input-field col s12">
+          <input value="Precio" id="Precio" type="text" class="validate" name="precio">
+          <label for="disabled">Precio</label>
+          <strong class="#d81b60 pink darken-1 ">{{ $errors->first('precio') }}</strong>
+        </div>
+      </div>
+
 <div class="row">
   <div class="col s8 input-field">
     <select name="marca" id="marca">
-        <option>
+        <option value="">
           Elija su marca
         </option>
         @foreach($marcas as $marca)
           <option Value="{{ $marca->id }}">{{ $marca->nombre }}</option>
         @endforeach
     </select>
+    <label>Elija marca</label>
+    <strong class="#d81b60 pink darken-1 ">{{ $errors->first('marca') }}</strong>
   </div>
 </div>
 <div class="row">
@@ -38,36 +59,32 @@
 
 <select name="categoria" id="categoria">
   @foreach($categorias as $categoria)
+  <option Value="">Elija Categoria</option>
   <option Value="{{ $categoria->id }}" >
     {{ $categoria->nombre}}
   </option>
   @endforeach
 </select>
+<label >Elija Categoria</label>
+<strong class="#d81b60 pink darken-1 ">{{ $errors->first('categoria') }}</strong>
 </div>
 </div>
-
-      <div class="row">
-        <div class="input-field col s12">
-          <input value="Precio" id="Precio" type="text" class="validate" name="Precio">
-          <label for="disabled">Precio</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="password" type="password" class="validate">
-          <label for="password">Password</label>
-        </div>
-      </div>
+    
       <div class="row">
       <div class="file-field input-field">
       <div class="btn">
         <span>Ingresar Imagen</span>
-        <input type="file">
+        <input type="file" name="imagen" multiple>
       </div>
+      <strong class="#d81b60 pink darken-1 ">{{ $errors->first('imagen') }}</strong>
       <div class="file-path-wrapper">
-        <input class="file-path validate" type="text">
+        <input class="file-path validate" 
+        type="text"
+        placeholder="Upload one or more files">
       </div>
-      <button class="btn waves-effect waves-light" type="submit" name="action">Submit</button>
+      </div>
+      </div>
+      <button class="btn waves-effect waves-light" type="submit" name="action">Enviar</button>
 
     </form>
  
